c1127d07a72a829532dd612a6f61d668b6851ccf
Eric Jacobson <contact@efjacobson.com>
2020-08-23 20:34:56 -0700

    getting pretty schwifty in here

--- keyboards/keebio/quefrency/keymaps/efjacobson/keymap.c
+++ keyboards/keebio/quefrency/keymaps/efjacobson/keymap.c
@@ -6,52 +6,150 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _BASE 0
-#define _FN1 1
+#define MAC_0 0
+#define MAC_1 1
+#define MAC_2 2
+
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  SWORDL,
+  SWORDR,
+  GUI_X,
+  GUI_C,
+  GUI_V,
+  GUI_Z,
+  GUI_SH_V,
+  ALT_TAB,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_65_with_macro(
-    KC_F1,   KC_F2,   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_MEDIA_PLAY_PAUSE, \
-    KC_F3,   KC_F4,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+  [MAC_0] = LAYOUT_65_with_macro(
+    KC_F1,   KC_ESC,  KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_MEDIA_PLAY_PAUSE, \
+    RGB_TOG,   RGB_MOD,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
     KC_F5,   KC_F6,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
     KC_F7,   KC_F8,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_F9,   KC_F10,  KC_LCTL, KC_LALT, KC_LGUI, MO(_FN1),KC_SPC,           MO(_FN1),KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F9,   KC_F10,  KC_LCTL, KC_LALT, KC_LGUI, MO(MAC_1),KC_SPC,           MO(MAC_1),KC_SPC,  MO(MAC_1), KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [_FN1] = LAYOUT_65_with_macro(
+  [MAC_1] = LAYOUT_65_with_macro(
     _______, _______, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
-    _______, _______, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, ALT_TAB, RGB_MOD, _______, _______,   _______, _______, _______, _______, KC_UP, _______, _______, _______, _______, _______, _______, \
+    _______, _______, MO(MAC_2), _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, \
+    _______, _______, _______, GUI_Z, GUI_X, GUI_C, GUI_V, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
+  [MAC_2] = LAYOUT_65_with_macro(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, SWORDL, _______, SWORDR, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!record->event.pressed) {
+        return true;
+    }
+    switch (keycode) {
+        case GUI_Z:
+            register_code(KC_LGUI);
+            tap_code(KC_Z);
+            unregister_code(KC_LGUI);
+            break;
+        case GUI_X:
+            register_code(KC_LGUI);
+            tap_code(KC_X);
+            unregister_code(KC_LGUI);
+            break;
+        case GUI_C:
+            register_code(KC_LGUI);
+            tap_code(KC_C);
+            unregister_code(KC_LGUI);
+            break;
+        case GUI_V:
+            register_code(KC_LGUI);
+            tap_code(KC_V);
+            unregister_code(KC_LGUI);
+            break;
+        case SWORDL:
+            register_code(KC_LALT);
+            register_code(KC_LSFT);
+            tap_code(KC_LEFT);
+            unregister_code(KC_LSFT);
+            unregister_code(KC_LALT);
+            break;
+        case SWORDR:
+            register_code(KC_LALT);
+            register_code(KC_LSFT);
+            tap_code(KC_RGHT);
+            unregister_code(KC_LSFT);
+            unregister_code(KC_LALT);
+            break;
+    }
+    return true;
+}
+
+void matrix_scan_user(void) {
+    if (is_alt_tab_active) {
+        if (timer_elapsed(alt_tab_timer) > 750) {
+            unregister_code(KC_LGUI);
+            is_alt_tab_active = false;
         }
     }
-    else if (index == 1) {
-      if (IS_LAYER_ON(_BASE)) {
-          if (clockwise) {
-              tap_code(KC_VOLU);
-          } else {
-              tap_code(KC_VOLD);
-          }
-      } else if (IS_LAYER_ON(_FN1)) {
-          if (clockwise) {
-              tap_code(KC_MEDIA_NEXT_TRACK);
-          } else {
-              tap_code(KC_MEDIA_PREV_TRACK);
-          }
-      }
-      }
+}
+
+void left_encoder(bool clockwise) {
+    switch (biton32(layer_state)) {
+        case 1:
+            register_code(KC_LCTRL);
+            if (!clockwise) {
+                register_code(KC_LSHIFT);
+            }
+            tap_code(KC_TAB);
+            if (!clockwise) {
+                unregister_code(KC_LSHIFT);
+            }
+            unregister_code(KC_LCTRL);
+            break;
+        case 0:
+        default:
+            if (!is_alt_tab_active) {
+                is_alt_tab_active = true;
+                register_code(KC_LGUI);
+            }
+            alt_tab_timer = timer_read();
+            if (!clockwise) {
+                register_code(KC_LSHIFT);
+            }
+            tap_code(KC_TAB);
+            if (!clockwise) {
+                unregister_code(KC_LSHIFT);
+            }
+            break;
+    }
+}
+
+void right_encoder(bool clockwise) {
+    switch (biton32(layer_state)) {
+        case 1:
+            tap_code(clockwise ? KC_MEDIA_NEXT_TRACK : KC_MEDIA_PREV_TRACK);
+            break;
+        case 0:
+        default:
+            tap_code(clockwise ? KC_VOLU : KC_VOLD);
+            break;
+    }
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        left_encoder(clockwise);
+    } else if (index == 1) {
+        right_encoder(clockwise);
+    }
 }
