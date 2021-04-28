4fb62b547d37c2cf8176574a7f9539ee4dd259ee
Eric Jacobson <contact@efjacobson.com>
2021-04-27 19:26:35 -0700

    down with the sickness

--- keyboards/keebio/quefrency/keymaps/efjacobson/keymap.c
+++ keyboards/keebio/quefrency/keymaps/efjacobson/keymap.c
@@ -9,9 +9,11 @@ extern keymap_config_t keymap_config;
 #define MAC_0 0
 #define MAC_1 1
 #define MAC_2 2
+#define GAMING_0 3
 
 bool is_alt_tab_active = false;
 uint16_t alt_tab_timer = 0;
+bool is_gaming = false;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -20,6 +22,7 @@ enum custom_keycodes {
   SLEFT,
   SRIGHT,
   CLUT,
+  GAMING,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,      KC_F2,      KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_END,     \
     KC_F3,      KC_F4,      KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,     KC_PGUP,    \
     KC_F5,      KC_F6,      KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_UP,      KC_PGDN,    \
-    RGB_TOG,    RGB_MOD,    KC_LCTL,    KC_LALT,    MO(MAC_1),  KC_LGUI,    MO(MAC_1),              _______,    KC_SPC,     MO(MAC_1),  KC_RCTL,    KC_RGUI,    KC_LEFT,    KC_DOWN,    KC_RGHT
+    GAMING,     RGB_MOD,    KC_LCTL,    KC_LALT,    MO(MAC_1),  KC_LGUI,    MO(MAC_1),              _______,    KC_SPC,     MO(MAC_1),  KC_RCTL,    KC_RGUI,    KC_LEFT,    KC_DOWN,    KC_RGHT
   ),
 
   [MAC_1] = LAYOUT_65_with_macro(
@@ -36,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    _______,    CLUT,       _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_UP,      _______,    _______,    _______,    _______,    _______,    _______,    \
     _______,    _______,    MO(MAC_2),  _______,    _______,    SLEFT,      SRIGHT,     _______,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______,    _______,    _______,    \
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    \
-    RGB_TOG,    RGB_MOD,    _______,    _______,    _______,    _______,    _______,                _______,    MO(MAC_2),  _______,    _______,    _______,    _______,    _______,    _______
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______,    MO(MAC_2),  _______,    _______,    _______,    _______,    _______,    _______
   ),
 
   [MAC_2] = LAYOUT_65_with_macro(
@@ -45,6 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    SWORDL,     KC_PGDN,    SWORDR,     _______,    _______,    _______,    _______,    \
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    \
     _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
+  ),
+
+  [GAMING_0] = LAYOUT_65_with_macro(
+    _______,    KC_ESC,     KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_DEL,     KC_BSPC,    KC_MEDIA_PLAY_PAUSE, \
+    KC_F1,      KC_F2,      KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_END,     \
+    KC_F3,      KC_F4,      KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,     KC_PGUP,    \
+    KC_F5,      KC_F6,      KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_UP,      KC_PGDN,    \
+    GAMING,     RGB_MOD,    KC_LCTL,    KC_LALT,    MO(MAC_1),  KC_LGUI,    KC_SPC,                 _______,    KC_SPC,     MO(MAC_1),  KC_RCTL,    KC_RGUI,    KC_LEFT,    KC_DOWN,    KC_RGHT
   )
 };
 
@@ -81,6 +92,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             tap_code(KC_RGHT);
             unregister_code(KC_LSFT);
             unregister_code(KC_LALT);
+            break;
+        case GAMING:
+            if (is_gaming) {
+                is_gaming = false;
+                rgblight_disable_noeeprom();
+                layer_off(GAMING_0);
+            } else {
+                is_gaming = true;
+                rgblight_enable_noeeprom();
+                layer_on(GAMING_0);
+            }
             break;
     }
     return true;
